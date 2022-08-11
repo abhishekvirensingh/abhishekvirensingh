54d3c2eb0b393e3d6ce6cc6c515791d503f414d4
Abhishek Singh <87334156+abhishekvirensingh@users.noreply.github.com>
2022-08-11 22:39:28 +0530

    Restructuring Project

--- general_pyspark_python_codes/general_codes/pyspark_codes/broadcast_variable.py
+++ general_pyspark_python_codes/general_codes/pyspark_codes/broadcast_variable.py
@@ -11,7 +11,7 @@ from pyspark import SparkContext
 
 def convert_to_set():
     boring_set = set()
-    file_source = open("DataFiles/boring_words.txt", "r")
+    file_source = open("../DataFiles/boring_words.txt", "r")
     file_content = file_source.read().splitlines()
     for each_word in file_content:
         boring_set.add(each_word)
@@ -21,7 +21,7 @@ def convert_to_set():
 sc = SparkContext("local[*]", "broadcast")
 broad_set = sc.broadcast(convert_to_set())  # broadcast the set to each spark driver
 
-data = sc.textFile("DataFiles/big_data_campaign_data.csv")
+data = sc.textFile("../DataFiles/big_data_campaign_data.csv")
 
 required_content = data.map(lambda x: (float(x.split(",")[10]), x.split(",")[0]))
 final_tuple = required_content.flatMapValues(lambda x: x.split(" ")).map(lambda x: (x[1].lower(), x[0]))
