6b20967d8a7430c9777b588de6a2cdc11ad4ae4b
Abhishek Singh <87334156+abhishekvirensingh@users.noreply.github.com>
2022-08-11 23:08:02 +0530

    Adding files for simple aggregation demo.

---
+++ general_pyspark_python_codes/general_codes/pyspark_aggregations/simple_aggregation.py
@@ -0,0 +1,7 @@
+# This demo will include how to perform simple aggregations in PySpark.
+# We will be loading the data file order_data.csv and will calculate :
+# total_number_of_rows, total_quantity,avg_unit_price,number_of_unique_invoices
+# we will perform simple aggregations using :
+# 1> Column Object Expression
+# 2> String Expressions
+# 3> Spark SQL
