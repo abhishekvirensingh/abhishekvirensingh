a09e984a28a54773b57286ce2a764fb987a2d007
Abhishek Singh <87334156+abhishekvirensingh@users.noreply.github.com>
2022-08-12 16:43:51 +0530

    Adding demo for simple aggregation

--- general_pyspark_python_codes/general_codes/pyspark_aggregations/simple_aggregation.py
+++ general_pyspark_python_codes/general_codes/pyspark_aggregations/simple_aggregation.py
@@ -5,3 +5,49 @@
 # 1> Column Object Expression
 # 2> String Expressions
 # 3> Spark SQL
+
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark_conf = SparkConf().setAppName("simple_aggregate").setMaster("local[*]")
+spark = SparkSession.builder.config(conf=spark_conf).getOrCreate()
+
+order_df = spark.read.format("csv") \
+    .option("path", "../DataFiles/order_data.csv") \
+    .option("inferSchema", True) \
+    .option("header", True).load()
+
+# order_df.printSchema()
+# order_df.show()
+# +---------+---------+--------------------+--------+---------------+---------+----------+--------------+
+# |InvoiceNo|StockCode|         Description|Quantity|    InvoiceDate|UnitPrice|CustomerID|       Country|
+# +---------+---------+--------------------+--------+---------------+---------+----------+--------------+
+# |   536378|     null|PACK OF 60 DINOSA...|      24|01-12-2010 9.37|     0.55|     14688|United Kingdom|
+# |   536378|     null|PACK OF 60 PINK P...|      24|01-12-2010 9.37|     0.55|     14688|United Kingdom|
+
+# total_number_of_rows, total_quantity,avg_unit_price,number_of_unique_invoices using
+# Column Object Expression
+
+order_df.select(
+    count("*").alias("total_number_of_rows"),
+    sum("Quantity").alias("total_quantity"),
+    avg("UnitPrice").alias("avg_unit_price"),
+    countDistinct("InvoiceNo").alias("number_of_unique_invoices")
+).show()
+
+# total_number_of_rows, total_quantity,avg_unit_price,number_of_unique_invoices using
+# String Expression
+
+order_df.selectExpr("count(*) as total_number_of_rows", "sum(Quantity) as total_quantity",
+                    "avg(UnitPrice) as avg_unit_price",
+                    "count(distinct(InvoiceNo)) as number_of_unique_invoices").show()
+
+# total_number_of_rows, total_quantity,avg_unit_price,number_of_unique_invoices using
+# spark sql
+
+order_df.createOrReplaceTempView("orders")
+spark.sql("select count(*) ,sum(Quantity) , avg(UnitPrice) , count(distinct(InvoiceNo)) from orders") \
+    .toDF("total_number_of_rows", "total_quantity", "avg_unit_price", "number_of_unique_invoices").show()
+
+spark.stop()
